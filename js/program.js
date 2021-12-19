5a53a156dcd1306fed204a0a5c8cdf9345d1ad31
GehadEddin <cihadeddink@gmail.vom>
2021-12-19 22:25:27 +0300

    konutlar divini ekledim ve diğer sayfaları oluştudum

---
+++ js/program.js
@@ -0,0 +1,36 @@
+$("#container").hover(function(){
+    $("#container .content ").show();
+  });
+  $(" #container").mouseleave(function(){
+    $("#container .content").hide();
+  }); 
+  $(" .cont").hover(function(){
+    $("  .content1 ").show();
+  });
+  $(".cont").mouseleave(function(){
+    $(" .content1").hide();
+  });
+  $(".cont1").hover(function(){
+    $(" .content2 ").show();
+  });
+  $(" .cont1").mouseleave(function(){
+    $(".content2").hide();
+  });
+  $(".cont2").hover(function(){
+    $(".content3 ").show();
+  });
+  $(" .cont2").mouseleave(function(){
+    $(" .content3").hide();
+  });
+  $(".cont3").hover(function(){
+    $(".content4 ").show();
+  });
+  $(" .cont3").mouseleave(function(){
+    $(" .content4").hide();
+  });
+  $("#container1").hover(function(){
+    $(".content5 ").show();
+  });
+  $(" #container1").mouseleave(function(){
+    $(".content5").hide();
+  });
